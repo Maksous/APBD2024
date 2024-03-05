9068409cd6cb52cd78ebb1a8744145aaa58cc29e
Maksous <maksous@pjwstk.edu.pl>
2024-03-05 13:23:11 +0100

    My first commit'

---
+++ FirstAPBDapp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            if (int.TryParse(Console.ReadLine(), out int userInput)) {
+                if(userInput>0)
+                {
+                    Console.WriteLine("The value is a positive one");
+                }else if (userInput < 0)
+                {
+                    Console.WriteLine("The value is a negative one");
+                }
+                else
+                {
+                    Console.WriteLine("The value is equal to 0");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer");
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
