52da726da0ea52effece2e86cc7242e474275080
maksous <maksous@pjwstk.edu.pl>
2024-04-23 14:14:43 +0200

    fixed missing semicolon

--- Task6/Task6/Models/Order.cs
+++ Task6/Task6/Models/Order.cs
@@ -3,7 +3,7 @@
     public class Order
     {
         public int IdOrder { get; set; }
-        public int ProductId { get; set}
+        public int ProductId { get; set; }
         public int Amount { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime FullfilledAt { get; set; }
