58fbcee1a9aed659f06443d513db7163b8db1641
Maksous <maksous@pjwstk.edu.pl>
2024-05-07 12:19:39 +0200

    Task7

---
+++ Task7/LinqCwiczenia/LinqTutorials/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+
+        }
+    }
+}
